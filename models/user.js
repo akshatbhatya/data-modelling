c49502bccf3922b66e8c87dc1570d5e9d44b84ea
akshat <akshatbhatya@gmail.com>
2024-03-23 16:09:01 +0530

    add new data add new data

--- models/user.js
+++ models/user.js
@@ -3,12 +3,19 @@ import moongoose from 'mongoose';
 let userData=new moongoose.Schema({
     name:{
         type:String,
+        lowercase:true,
         required:[true,"name is required"]
     },
     email:{
         type:String,
+        lowercase:true,
         unique:true,
         required:[true,"email is required"]
+    },
+    password:{
+        type:String,
+        required:[true,"password is required"]
     }
-    
 },{timeStamp:true})
+
+export const userSchema=moongoose.model("userSchema",userData);
